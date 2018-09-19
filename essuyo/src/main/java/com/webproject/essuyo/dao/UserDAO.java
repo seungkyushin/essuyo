0daffb5d1a003a2326aad1c6c5d8254e6d8ecb20
seoWon <seoWon@DESKTOP-084J7JT>
2018-09-19 15:46:38 +0900

    UserDAO에 주석 추가

--- essuyo/src/main/java/com/webproject/essuyo/dao/UserDAO.java
+++ essuyo/src/main/java/com/webproject/essuyo/dao/UserDAO.java
@@ -22,7 +22,7 @@ public class UserDAO {
 	//네임스페이스에 매퍼 주소 변경해 줘야 함
 	private static String namespace = "com.spring.mapper.UserMapper";
 
-	
+	//로그인 기능을 해 준다. vo 객체에서 이메일과 비번을 받아서 dto에 배열로 넣어줌.
 	public UserVO login(UserVO vo) throws Exception {
 		
 		String dto[] = {vo.getEmail(), vo.getPw()};
@@ -30,6 +30,7 @@ public class UserDAO {
 	}
 
 	
+	//로그인 유지 기능. 여유가 생기면 마저 손보고, 작동 안 하는데 시간이 없을 경우엔 삭제
 	public void keepLogin(String email, String seesionId, Date next) {
 		Map<String, Object> paraMap = new HashMap<>();
 		paraMap.put("email", email);
@@ -40,7 +41,7 @@ public class UserDAO {
 		
 	}
 
-	
+	// 세션키 체크 기능, 위의 로그인 유지 기능과 마찬가지
 	public UserVO checkUserWithSessionkey(String value) {
 		
 		return session.selectOne(namespace+".checkUserWithSessionkey", value);
