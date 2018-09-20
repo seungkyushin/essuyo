9a0e83e759965e0cc15489c5b4c0cdeea3b48f55
seoWon <seoWon@DESKTOP-084J7JT>
2018-09-20 17:24:02 +0900

    유저 컨트롤러, 회원가입 추가, 스타일, regist에 유효성 검사 관련 태그 추가

--- essuyo/src/main/java/com/webproject/essuyo/controller/UserController.java
+++ essuyo/src/main/java/com/webproject/essuyo/controller/UserController.java
@@ -1,8 +1,40 @@
 package com.webproject.essuyo.controller;
 
+import javax.inject.Inject;
+import javax.servlet.http.HttpSession;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import com.webproject.essuyo.domain.UserVO;
+import com.webproject.essuyo.service.UserService;
 
 @Controller
+@RequestMapping("/user/*")
 public class UserController {
+	private static final Logger logger = LoggerFactory.getLogger(UserController.class);
+	
+	@Inject
+	private UserService service;
+	
+	//GET 방식으로 회원가입 페이지에 접근. 그냥 회원가입 페이지로 보내준다
+	@RequestMapping(value="/regist", method=RequestMethod.GET)
+	public void registGet(UserVO vo, Model model) throws Exception {
+		logger.info("registGet.......");
+	}
 
+	
+	//POST 방식으로 회원가입 페이지 접근, form에 입력된 정보들은 받아야 하는데...
+	@RequestMapping(value="/regist", method=RequestMethod.POST)
+	public String registPost(UserVO vo, HttpSession session, Model model) throws Exception{
+		
+		service.regist(vo);
+		//회원가입이 끝나면 메인 화면으로 리다이렉트
+		return "redirect:/main";
+		
+	}
 }
